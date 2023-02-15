69df38ba33c6fdbdfd4b7535184de27ff208ece6
Vikas Bhardwaj <vikas.bhardwaj@grazitti.local>
2023-02-15 19:09:11 +0530

    register member page

--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -1,21 +1,22 @@
-import Button from './../Buttons/Button';
-import BannerHead from './../general/Headings/BannerHead';
-import TrainerSchedule from './../Trainers/TrainerSchedule';
+// import Button from './../Buttons/Button';
+// import BannerHead from './../general/Headings/BannerHead';
+// import TrainerSchedule from './../Trainers/TrainerSchedule';
 const Homepage = () => {
     return (
         <>
             <div className="container-fluid bg-primary p-5 bg-hero mb-5">
                 <div className="row py-5">
                     <div className="col-12 text-center">
-                        <BannerHead headText="Classes"></BannerHead>
+                        {/* <BannerHead headText="Classes"></BannerHead> */}
                         <div className="d-flex justify-content-center">
-                            <Button gap="2" btnName="JOIN US"></Button>
-                            <Button gap="0" varient="light" btnName="CONTACT US"></Button>
+                            Test
+                            {/* <Button gap="2" btnName="JOIN US"></Button>
+                            <Button gap="0" varient="light" btnName="CONTACT US"></Button> */}
                         </div>
                     </div>
                 </div>
             </div>
-            <TrainerSchedule />
+            {/* <TrainerSchedule /> */}
         </>
     )
 }
